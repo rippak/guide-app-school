e561f3813668dce6a05aab934c9e1e87bd6bda46
Rippak <rippak1986@gmail.com>
2025-03-19 00:45:08 +0300

    Подключение карт

--- guide-app/src/main.ts
+++ guide-app/src/main.ts
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createYmaps } from 'vue-yandex-maps';
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(createYmaps({
+    apikey: import.meta.env.VITE_YMAPS_API_KEY,
+    lang: 'ru_RU',
+}));
 
 app.mount('#app')
